fc2eeef9640453e0ede38aa8d5058dc420cc5f62
teissi_m <teissi_m@etna-alternance.net>
2020-03-11 14:43:10 +0100

    commit

---
+++ android/app/src/components/PhotoCamera.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Text from 'react-native';  
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'black',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    topButtons: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        alignItems: 'flex-start',
+    },
+    bottomButtons: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    flipButton: {
+        flex: 1,
+        marginTop: 20,
+        right: 20,
+        right: 20,
+        alignSelf: 'flex-end',
+    },
+    recordingButton: {
+        marginBottom: 10,
+    },
+});
+
+class PhotoCamera extends React.PureComponent {
+    state = {
+        type: RNCamera.Constants.Type.back,
+    };
+
+    flipCamera = () =>
+        this.setState({
+            type:
+                this.state.type === RNCamera.Constants.Type.back
+                    ? RNCamera.Constants.Type.front
+                    : RNCamera.Constants.Type.back,
+        });
+    
+    takePhoto = async () => {
+        const { onTakePhoto } = this.props;
+        const options = {
+            quality: 0.5,
+            base64: true,
+            width: 300,
+            height: 300,
+        };
+        const data = await this.flipCamera.takePictureAsync(options);
+        onTakePhoto(data.base64);
+    };
+    render() {
+        const { type } = this.state;
+        return (
+            <View style={styles.container}>
+                <RNCamera
+                    ref={cam => {
+                        this.camera = cam;
+                    }}
+                    type={type}
+                    style={styles.preview}
+                />
+                <View style={styles.topButtons}>
+                    <TouchableOpacity onPress={this.flipCamera} style={styles.flipButton}>
+                        <Text test />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.bottomButtons}>
+                    <TouchableOpacity onPress={this.takePhoto} style={styles.recordingButton}>
+
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default PhotoCamera;
--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import RNCamera from 'react-native-camera';
 import App from './app/App';
 import {name as appName} from './app.json';
+import PhotoCamera from './android/app/src/components/PhotoCamera'
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(PhotoCamera, () => App);
