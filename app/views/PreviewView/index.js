9d2c406d73ec18c2839b961ba829df33083a5149
Manuel <manuel.teissier@gmail.com>
2020-03-12 22:05:24 +0100

    title and description, no API call yet

--- app/views/PreviewView/index.js
+++ app/views/PreviewView/index.js
@@ -10,29 +10,98 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { faShareSquare } from '@fortawesome/free-solid-svg-icons'
+import Video from 'react-native-video';
+
 
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacity, 
+  TextInput
 } from 'react-native';
+import { Fragment } from 'react';
 
 class PreviewView extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: null,
+            video: null,
+            description: null,
+            location: null
+        }
+    }
+
+    handleTitle = (text) => {
+        this.setState({ title: text })
+    }
+
+    handleDescription = (text) => {
+        this.setState({ description: text })
+    }
+
+    handleLocation = (text) => {
+        this.setState({ location: text })
+    }
+
+    sendVideo(title, description, video) {
+       // 'title': text, 'description': text
+    }
+
     render() {
         const { urlVideo } = this.props.route.params
         return (
             <View>
-                <Text>{urlVideo}</Text>
-                <FontAwesomeIcon icon={ faShareSquare } style={style.icon} />
+                {urlVideo && (
+                    <React.Fragment>
+                        <TextInput style = {styles.input}
+                            underlineColorAndroid = "transparent"
+                            placeholder = "Title"
+                            placeholderTextColor = "#9a73ef"
+                            autoCapitalize = "none"
+                            onChangeText = {this.handleTitle}/>
+
+                        <TextInput style = {styles.input}
+                            underlineColorAndroid = "transparent"
+                            placeholder = "Description"
+                            placeholderTextColor = "#9a73ef"
+                            autoCapitalize = "none"
+                            onChangeText = {this.handleDescription}/>
+
+                        <Video source={{uri: urlVideo}}   // Can be a URL or a local file.
+                            ref={(ref) => {
+                            this.player = ref
+                            }}                                      // Store reference
+                            onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                            onError={this.videoError}               // Callback when video cannot be loaded
+                            style={styles.backgroundVideo} />
+                        <TouchableOpacity style = {styles.submitButton} 
+                            onPress = {() => this.sendVideo(this.state.title, this.state.description, this.state.video)}>
+                            <FontAwesomeIcon icon={ faShareSquare } style={styles.icon} />
+                        </TouchableOpacity>
+                    </React.Fragment>
+                )}
             </View>
         )
     }
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     icon: {
       color: 'blue'
-    }
+    },
+    backgroundVideo: {
+        width: 400,
+        height: 400
+    },
+    input: {
+        margin: 15,
+        height: 40,
+        borderColor: '#7a42f4',
+        borderWidth: 1
+     },
 })
 
 export default PreviewView;
