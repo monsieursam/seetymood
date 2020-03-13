1c3cda72fb829d055303c291797627d915f9f08c
Sam <sam.mamane@hotmail.fr>
2020-03-13 09:44:50 +0100

    Add loading

--- app/views/FeedView/index.js
+++ app/views/FeedView/index.js
@@ -13,18 +13,18 @@ import {
   StyleSheet,
   FlatList,
   ScrollView,
+  RefreshControl,
   TouchableOpacity
 } from 'react-native';
 import { getAllVideo } from '../../api/api';
-
-import Video from 'react-native-video';
 
 class FeedView extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            allVideos: null
+            allVideos: null,
+            isFetching: false,
         }
 
     }
@@ -33,6 +33,10 @@ class FeedView extends Component {
         this.getAllVideos()
         
     }
+
+    onRefresh() {
+        this.setState({ isFetching: true }, function() { this.getAllVideos() });
+     }
 
     goToVideo = (id) => {
         console.log(id)
@@ -45,7 +49,8 @@ class FeedView extends Component {
             const response = await fetch('https://hackathon.seetymood.com/api/videos');
             const responseJson = await response.json();
 
-            this.setState({allVideos: responseJson})
+            this.setState({allVideos: responseJson, isFetching:false})
+
         }
         catch (error) {
             console.error(error);
@@ -68,39 +73,20 @@ class FeedView extends Component {
     )
 
     render() {
-        const { allVideos } = this.state
+        const { allVideos, isFetching } = this.state
     
         console.log(allVideos)
         return (
         <View style={styles.container}>
             {allVideos && <FlatList 
+                onRefresh={() => this.onRefresh()}
                 keyExtractor={this.renderMyKeyExtractor}
                 data={allVideos} 
                 style={styles.scrollView} 
                 renderItem={this.renderMyList}
+                refreshing={isFetching}
                 />
             }
-                {/* {allVideos && allVideos[0] && (
-                    <Video source={{uri: allVideos[0].file}}   // Can be a URL or a local file.
-                        ref={(ref) => {
-                            this.player = ref
-                        }}                                      // Store reference
-                        onBuffer={this.onBuffer}                // Callback when remote video is buffering
-                        onError={this.videoError}               // Callback when video cannot be loaded
-                        style={styles.backgroundVideo} />
-                )}
-                {allVideos && !allVideos.map(video => video.file && (
-                <View key={video.id}>
-                    {console.log(video.file)}
-                    <Text>{video.description}</Text>
-                    <Video source={{uri: video.file}}   // Can be a URL or a local file.
-                        ref={(ref) => {
-                            this.player = ref
-                        }}                                      // Store reference
-                        onBuffer={this.onBuffer}                // Callback when remote video is buffering
-                        onError={this.videoError}               // Callback when video cannot be loaded
-                        style={styles.backgroundVideo} />
-                </View>))} */}
                 
         </View>
         )
